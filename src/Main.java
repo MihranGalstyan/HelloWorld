0ec618465e1fcfdd098a49fe86aa0085fed33181
Mihran Galstyan <msho.mher@gmail.com>
2022-09-23 02:44:01 +0400

    Update project

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Mihran Galstyan
+ * All rights reserved
+ */
+public class Main {
+    public static void main(final String[] args) {
+        System.out.println("Hello World!!!");
+    }
+}
